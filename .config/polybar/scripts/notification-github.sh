eced7597a5baf2ee472647272fb47a6bdc2eceaa
arahiko-ayami <zinang7@gmail.com>
2021-12-28 00:40:30 +0700

    remove unnecessary content

--- .config/polybar/scripts/notification-github.sh
+++ .config/polybar/scripts/notification-github.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 USER="arahiko-ayami"
-TOKEN="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"
+TOKEN=""
 notifications=$(echo "user = \"$USER:$TOKEN\"" | curl -sf -K- https://api.github.com/notifications | jq ".[].unread" | grep -c true)
 
 if [ "$notifications" -gt 0 ]; then
